dcb6d38d31724c0eb9d2c47ae205072425cf4a3d
Nick Smith <nick@nicksmith.org>
2018-03-28 23:34:22 +0100

    Draw thicker lines and circles

--- ww.py
+++ ww.py
@@ -10,6 +10,7 @@ IMAGE_SIZE = 400
 FONT_FILE = 'leaguespartan-bold.ttf'
 centre_font_size = int(IMAGE_SIZE/6)
 radial_font_size = int(IMAGE_SIZE/7)
+line_width = int(IMAGE_SIZE/100)
 
 diameter = 0.9  # as proportion of image size
 
@@ -22,12 +23,18 @@ parser.add_argument('--verbatim', help="Use the letters in the order given, anti
 args = parser.parse_args()
 
 
-def draw_circle(draw_object, image_size, diameter, fill, outline):
+def draw_circle(draw_object, image_size, diameter, fill, outline, thickness=1):
     border = image_size * (1 - diameter) / 2
     draw_object.ellipse(
         [
             (border, border),
             (image_size - border, image_size - border),
+        ], fill=outline, outline=outline
+    )
+    draw_object.ellipse(
+        [
+            (border + thickness, border + thickness),
+            (image_size - border - thickness, image_size - border - thickness),
         ], fill=fill, outline=outline
     )
 
@@ -42,8 +49,8 @@ im = Image.new('RGB', (IMAGE_SIZE, IMAGE_SIZE), (255, 255, 255))
 d = ImageDraw.Draw(im)
 
 # Draw the circles
-draw_circle(d, IMAGE_SIZE, diameter, (255, 255, 255), 0)  # outer circle
-draw_circle(d, IMAGE_SIZE, diameter / 3, (200, 200, 200), 0)  # inner circle
+draw_circle(d, IMAGE_SIZE, diameter, (255, 255, 255), 0, line_width)  # outer circle
+draw_circle(d, IMAGE_SIZE, diameter / 3, (200, 200, 200), 0, line_width)  # inner circle
 
 # Draw the spokes
 spoke_end_coordinates = [
@@ -57,7 +64,7 @@ spoke_end_coordinates = [
 ]
 
 for ((x1, y1), (x2, y2)) in spoke_end_coordinates:
-    d.line(((x1, y1), (x2, y2)), fill="black")
+    d.line(((x1, y1), (x2, y2)), fill="black", width=line_width)
 
 # Get centre coordinates of the outer letters
 radial_letter_coordinates = [
